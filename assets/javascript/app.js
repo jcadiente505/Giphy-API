71e737fafd29d034f74ff778f9870d3d4f210723
Cadman <jcadiente505@gmail.com>
2018-06-20 00:07:04 -0400

    completed function display button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,40 @@
 
 var topics = ["Jim Carrey", "Dwayne Johnson", "Brad Pitt"];
+var gifs = "";
 
-var queryURL = "https://api.giphy.com/v1/gifs/search?" + topics + "&api_key=WQ4ApiFaUDoztB9gT5Es6XglEjXI3acX&limit=10"
-console.log(queryURL)
-$.ajax({
-    url: queryURL,
-    method: "GET"
-}).then(function (response){
+// this Function will display all buttons at the top of the page
+function actorBtns() {
 
-    var gifs = $("#gifsection");
-    var rating = response.rating;
+    $("#search-btn").val().trim();
 
-    $("#search-btn").on("click")
-});
+    for (i = 0; i < topics.length; i++) {
+
+    var actorButton = $("<button class='btn btn-warning'>");
+    actorButton.addClass("actor")
+    actorButton.attr("type", "button");
+    actorButton.attr("data-name", topics[i])
+    actorButton.text(topics[i]);
+    $("#actorsection").append(actorButton);
+
+    };
+};
+
+actorBtns();
+// 
+$("actorbtn").on("click", function(){
+
+    var actor = $(this).attr("actorbtn");
+
+    var queryURL = "https://api.giphy.com/v1/gifs/search?" + actor + "&api_key=WQ4ApiFaUDoztB9gT5Es6XglEjXI3acX&limit=10"
+
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response){
+
+        console.log(queryURL);
+        console.log(response);
+    })
+})
+
+
