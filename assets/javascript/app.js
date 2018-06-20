40acf6cf3e62fa2e41f8db0c1d64d4fc088c8ef3
Cadman <jcadiente505@gmail.com>
2018-06-20 11:52:52 -0400

    added the displayButtons function and ajax method

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,111 +1,80 @@
+document.ready(function () {
+    // beginning actors array, we will add searched actors into here
+    var actors = ["Jim Carrey", "Dwayne Johnson", "Robin Williams"];
+    // function to display the gifs of actors
+    function gifDisplay() {
+        // grabs the data attribute from the selection
+        var actorName = $(this).data("actor");
+        console.log(actorName);
+        // Giphy API with a limit set too 10
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + actorName + "&api_key=WQ4ApiFaUDoztB9gT5Es6XglEjXI3acX&limit=10";
 
-var topics = ["Jim Carrey", "Dwayne Johnson", "Brad Pitt"];
-var gifs = "";
+        console.log(queryURL);
 
-// this Function will display all buttons at the top of the page
-function actorBtns() {
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            // store the data from the API'S response into a results variable
+            var results = response.data;
+            console.log(results)
+            // loop through the response.data 10 times **which we set in the URL**
+            for (var i = 0; i < results.length; i++) {
+                // store our HTML div where the actor gifs will go into a variable
+                var actorDiv = $("<div class='col-md-4'>");
+                // grab the rating for each response
+                var rating = results[i].rating;
+                // grab the src's for the still and active states
+                var activesrc = results[i].images.fixed_height.url;
+                var stillsrc = results[i].images.fixed_height_still.url;
+                // create an image tag for the gifs
+                var showGif = $("<img>");
+                // create a p tag for the rating **use the rating var** 
+                var p = $("<p>").text("Rating: " + rating)
 
-    $("#search-btn").val().trim();
-
-    for (var i = 0; i < topics.length; i++) {
-
-    var actorButton = $("<button class='btn btn-warning'>");
-    actorButton.addClass("actor")
-    actorButton.attr("data-name", topics[i])
-    actorButton.text(topics[i]);
-    $("#actorsection").append(actorButton);
-
+                // assign the attributes
+                // set the default too still
+                showGif.attr("src", stillsrc);
+                // add the actorGif class for dynamic buttons
+                showGif.addClass("actorGif");
+                // add both the still and active data state's too switch between
+                showGif.attr("data-state", "still");
+                showGif.attr("data-still", stillsrc);
+                showGif.attr("data-active", activesrc);
+                // append the content into our actorDiv
+                actorDiv.append(p);
+                actorDiv.append(showGif);
+                // append the newly created div into our gif section
+                $("#gifsection").append(actorDiv);
+            };
+        });
     };
-};
-// call the actorBtns function!
-actorBtns();
-// Click listener for the search button/adds the new actor into array and creates new button
-$("search-btn").on("click", function(event){
-    
-    event.preventDefault();
-// grabs the actor name from the search
-    var actorInput = $("#actor-search").val().trim();
-    var actorName = $(this).attr("data-name")
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?" + actorInput + "&api_key=WQ4ApiFaUDoztB9gT5Es6XglEjXI3acX&limit=4"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (response){
-
-        if (response.pagination.total_count >= 10) {
-// push the actor searched for into the topics array
-        topics.push(actorInput);
-// reload the buttons with new array items
-        actorBtns();
+    // function too loop through array and display buttons
+    function displayButtons() {
+        // loop through our array
+        for (var i = 0; i < actors.length; i++) {
+            // create a variable for our dynamic buttons
+            var actorBtn = $('<button class="btn btn-warning">');
+            actorBtn.attr("id", "actor");
+            // create data-search attribute
+            actorBtn.attr("data-search", actors[i]);
+            actorBtn.text(actors[i]);
+            // append the button into our html section
+            $("#actorsection").append(actorBtn);
         }
-// this is displayed if there werent any gifs to show
-        else if (response.pagination.total_count === 0 ) {
-            $("#gifsection").html("<h1>Sorry no results. Try a Different Actor!</h1>");
-        }
-// in case there is less than 10 gifs to show
-        else {
-            $("#gifsection").html("<h1>Sorry, there's only " + response.pagination.total_count + "Try a Different Actor!</h1>")
-        };
-        $("#actor-search").val(" ");
-    });
+    }
+
+
+    $("#actor-search").on("click", function (event) {
+
+        event.preventDefault();
+        // grab the actor the user searched for
+        var newActor = $("#actor-input").val().trim();
+        // push the content into our actor array
+        actors.push(newActor);
+        console.log(actors)
+        $("actor-input").val(" ");
+    })
 });
-
-$(document).on("click", ".actor", gifDisplay)
-
-function gifDisplay() {
-
-    var actorName = $(this).attr("data-name")
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?" + actorName + "&api_key=WQ4ApiFaUDoztB9gT5Es6XglEjXI3acX&limit=10"
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).done(function (response){ 
-// goes through the length of the giphy pictures which we set too 10
-        for (var j = 0; j < response.data.length; j++) {
-
-            var activeGif = response.data[j].images.fixed_width.url;
-            var stillGif = response.data[j].images.fixed_width_still.url;
-            var rating = "Rating: " + (response.data[j].rating).toUpperCase();
-
-            var actorGif = $("<img>");
-            var ratingsection = $("<div id='ratingsection'>" + rating + "</div>");
-// this sets the gif to the still state also adds the active state which we will switch on click
-            actorGif.attr({"active": active, "still": still, "src": still, "state": "still"});
-
-            var ratedGif = $("<div id=ratedgif>");
-            ratedGif.prepend(ratingsection, actorGif);
-// displays the gif w/ rating
-            $("#gifsection").prepend(ratedGif);
-
-// this will be the start/stop function of the gifs
-            $(actorGif).on("click", function(event){
-
-                var gifState = $(this).attr("state");
-                var gifsource = $(this).attr("src");
-// this conditional registers the state of the gif and changes from still to active on click
-                if (gifState === "still") {
-                    $(this).attr("src", $(this).attr("active"));
-                    $(this).attr("state", "active"); }
-                else {
-                    $(this).attr("src", $(this).attr("still"));
-                    $(this).attr("state", "still"); } 
-            });
-        };
-
-    });
-
-
-
-
-
-
-
-
-
-}
 
 
